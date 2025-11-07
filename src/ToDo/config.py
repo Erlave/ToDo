fdb628a034b9776088684eeda6f3ed041e77de10
Erlave <erlave.work@gmail.com>
2025-11-07 22:08:10 +0330

    change config

--- src/ToDo/config.py
+++ src/ToDo/config.py
@@ -1,18 +1,19 @@
 import sys
 from pathlib import Path
 
-APP_NAME = "ErlaveTodo"
+APP_NAME = "ErlaveToDo"
 APP_VERSION = "0.1.0"
 
-
-if getattr(sys, "_MEIPASS", False):
-    BASE_DIR = Path(sys._MEIPASS)
+# مسیر نصب (فقط برای حالت exe)
+if getattr(sys, 'frozen', False):
+    BASE_DIR = Path(sys.executable).parent
 else:
-    BASE_DIR = Path(__file__).resolve().parent.parent.parent
+    BASE_DIR = Path(__file__).resolve().parent.parent
 
+# مسیر امن برای دیتابیس (در AppData کاربر)
+APP_DIR = Path.home() / "AppData" / "Local" / APP_NAME
+DATA_DIR = APP_DIR / "data"
+DB_PATH = DATA_DIR / "todo.db"
 
-DATA_DIR = BASE_DIR / "data"
-DB_PATH = DATA_DIR /"todo.db" 
-
-# اگه data وجود نداشت، بساز
-DATA_DIR.mkdir(exist_ok=True)
+# ساخت پوشه‌ها در صورت نیاز
+DATA_DIR.mkdir(parents=True, exist_ok=True)
